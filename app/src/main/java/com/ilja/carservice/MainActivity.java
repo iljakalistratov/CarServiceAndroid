4c8a0d4f4d9b9dfed117a7e042b3ae7cf5ce15d2
Ilja <Ilja.Kalistratov@nttdata.com>
2020-11-26 07:55:17 +0100

    Swipe To Delete

--- app/src/main/java/com/ilja/carservice/MainActivity.java
+++ app/src/main/java/com/ilja/carservice/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -139,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements Adapter.ClickList
         getJsonData();
     }
 
-    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
             return false;
